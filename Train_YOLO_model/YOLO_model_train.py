14312d08d66386c1bf9081342ee2feb6bc580251
aashish3234 <161685783+aashish3234@users.noreply.github.com>
2026-03-24 13:43:40 +0530

    Change training dataset path for YOLO model

--- Train_YOLO_model/YOLO_model_train.py
+++ Train_YOLO_model/YOLO_model_train.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     from ultralytics import YOLO
     model1 = YOLO('yolov8n.pt')
 
-    model1.train(data="/kaggle/input/aquarium-data-cots/aquarium_pretrain/data.yaml",epochs=100,imgsz=640, batch=10)
+    model1.train(data="/kaggle/input/human-640/data.yaml",epochs=100,imgsz=640, batch=10)
